2d8e29444297efcca5ed78a6d9c4d73e259e15ec
Ali200119 <aliit@code.edu.az>
2023-02-06 19:06:05 +0400

    Conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,171 @@
 // const IS_MARRIED = false;
 
 // console.log(name, surname, IS_MARRIED);
+
+// console.log(name + " " + surname + " " + IS_MARRIED);
+
+// if(true){
+//     var age = 55;
+// }
+
+// console.log(age);
+
+// if(true){
+//     let age = 55;
+// }
+
+// console.log(age);
+
+// const age = 77;
+
+// age = 70;
+
+// console.log(age);
+
+// test1 = "Hey";
+
+// console.log(test1);
+
+// let name = "Cavid";
+
+// let age = 29;
+
+// let IS_TEACHER = true;
+
+// let itsUndefined;
+
+// let nul = null;
+
+
+
+// console.log(name);
+
+// console.log(age);
+
+// console.log(IS_TEACHER);
+
+// console.log(undefined);
+
+// console.log(null);
+
+
+
+// console.log(typeof name);
+
+// console.log(typeof age);
+
+// console.log(typeof IS_TEACHER);
+
+// console.log(typeof undefined);
+
+// console.log(typeof null);
+
+
+// if(itsUndefined == undefined){
+//     console.log("Is undefined");
+// }
+
+// let student = {
+//     name: "Ali",
+//     surname: "Talibov",
+//     age: 21
+// }
+
+// console.log(student);
+
+// console.log(`${student.name} ${student.surname} ${student.age}`);
+
+// let teachers = ["Cavid", "Ibrahim"];
+
+// console.log(teachers);
+
+// for (let index = 0; index < teachers.length; index++) {
+//     const element = teachers[index];
+//     console.log(element);
+// }
+
+
+// Let's say I want to take only numbers from array
+
+// let array = ["Test1", 1, 2, 3, 4, 5, true, null, undefined];
+
+// const number = typeof 7;
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+//     if (typeof element == number) {
+//         console.log(element);
+//     }
+// }
+
+// alert("Hello Cavid teacher");
+
+// let age = confirm("Are you over 18?");
+
+// console.log(age);
+
+// debugger;
+
+// let username = prompt("Please, enter your username");
+
+// console.log(username);
+
+// let isOnline = confirm("Are you online?");
+
+// if (isOnline == true) {
+//     console.log("You are online");
+// }
+
+// else {
+//     console.log("You are offline");
+// }
+
+// let age = 20;
+
+// if (age > 7) {
+//     console.log("Boyukdur");
+// }
+
+// else {
+//     console.log("Kicikdir");
+// }
+
+// let temp = 15;
+
+// if (temp < 10) {
+//     console.log("Soyuqdur");
+// }
+
+// else if (temp > 20) {
+//     console.log("Istidir")
+// }
+
+// else {
+//     console.log("Hecbisey");
+// }
+
+// let count = 7;
+
+// switch (count) {
+//     case 5:
+//         console.log("besh");
+//         break;
+
+//     case 6:
+//         console.log("alti");
+//         break;
+
+//     case 7:
+//         console.log("yeddi");
+//         break;
+
+//     default:
+//         console.log("Not found");
+//         break;
+// }
+
+// for (let i = 0; i <= 7; i++) {
+//     console.log(i);    
+// }
+
