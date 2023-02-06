726e7c5e1160e2af31b3d0e3dc77bd317fdde62d
Ali200119 <aliit@code.edu.az>
2023-02-06 15:03:39 +0400

    js external start

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,14 @@
 
 // console.log("Hi");
 
-alert("Hello there!");
+// alert("Hello there!");
+
+// var, let, const
+
+// var name = "Ali";
+
+// let surname = "Talibov";
+
+// const IS_MARRIED = false;
+
+// console.log(name, surname, IS_MARRIED);
